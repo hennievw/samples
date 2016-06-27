d48ebb12132a0ce3ebee7a1a59de19e0cddc0962
Hennie van Wyk <hennievw@outlook.com>
2016-06-27 21:21:09 +0200

    Added Grunt sample files

---
+++ Grunt/gruntfile.js
@@ -0,0 +1,31 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            options: {
+                compress: true
+            },
+            target: {
+                files: {
+                    "Scripts/App/components.min.js": [
+                    "Scripts/App/userlist.js",
+                    "Scripts/App/imgbanner.js",
+                    "Scripts/App/peoplepicker.js",
+                    ],
+                    "Scripts/App/app.min.js": [
+                    "Scripts/App/about.js",
+                    "Scripts/App/router.js",
+                    "Scripts/App/home.js",
+                    ]
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.registerTask("customTask", ["uglify"]);
+};
+
+
