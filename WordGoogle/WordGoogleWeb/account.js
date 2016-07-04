894330fc9178bffe3ab1e068c70a48f759bbbec5
Hennie van Wyk <hennievw@outlook.com>
2016-07-04 08:08:25 +0200

    Office Google auth sample
    -added source code for google addin blog sample

---
+++ WordGoogle/WordGoogleWeb/account.js
@@ -0,0 +1,81 @@
+﻿/**
+* Hennie van Wyk - codestep blog
+* Sample code for integrating an Office Addin with the Google API.
+* http://www.codestepblog.com/
+*/
+
+"use strict";
+
+var account = (function () {
+           
+    var clientId = "[YOUR CLIENTID COMES HERE]";
+    var scopes = "https://www.googleapis.com/auth/contacts.readonly";
+    var redirectUri = "https://localhost:44300/account.html";
+        
+    /**
+    * Retreive the hash segment of a URL
+    * @param The URL to parse
+    * @return The hash segment withouth the '#' characther
+    */
+    var getHash = function (hash) {
+        if (hash.indexOf("#") > -1) {
+            hash = hash.substring(1);
+        }
+        return hash;
+    }
+
+    /**
+    * Scans the hash of the URL redirected to by Google and deserialize it into an object
+    * @param The URL to parse
+    * @return The hash segment withouth the '#' characther
+    */
+    var parseResponse = function (hash) {
+        var params = {};
+        var regex = /([^&=]+)=([^&]*)/g, m;
+        while (m = regex.exec(hash)) {
+            params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+        }
+        return params;
+    }    
+
+    /**
+    * Check whether the page is a redirect from Google OAuth and if auth was successful
+    * @param A deserialized object representing the returned hash by Google OAuth
+    * @return True if auth was successful, false otherwise
+    */
+    var isAuthCallback = function (authResponse) {
+        if (typeof authResponse !== "undefined" && authResponse !== null) {
+
+            if (authResponse.hasOwnProperty("access_token")) {
+                return true;
+            } else if (authResponse.hasOwnProperty("error")) {
+                console.log("Authorization failed: " + authResponse.error);
+            }
+        }
+        return false;
+    }
+
+    /**
+    * Redirect to the Google OAuth page
+    */
+    var performAuthorization = function () {
+        window.location = "https://accounts.google.com/o/oauth2/v2/auth" 
+            + "?scope=" + scopes
+            + "&state=auth" 
+            + "&redirect_uri=" + redirectUri
+            + "&response_type=token" 
+            + "&client_id=" + clientId;
+    }    
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var hash = getHash(window.location.hash);
+            var authResponse = parseResponse(hash);
+            if (isAuthCallback(authResponse)) {
+                Office.context.ui.messageParent("signin#" + JSON.stringify(authResponse));
+            }
+            performAuthorization();
+        });
+    };
+        
+})(window.account || (window.account = {}))
---
+++ WordGoogle/WordGoogleWeb/home.js
@@ -0,0 +1,59 @@
+﻿/**
+* Hennie van Wyk - codestep blog
+* Sample code for integrating an Office Addin with the Google API.
+* http://www.codestepblog.com/
+*/
+
+"use strict";
+
+var app = (function () {
+
+    /**
+    * Print out the email addresses returned by the contacts API
+    */
+    var displayContactEmails = function (contacts) {
+        $("#login-view").hide();
+        
+        var contactList = $("#contacts-list");
+        $.each(contacts, function (index, value) {
+            $("#contacts-list").append("<li>" + value.gd$email[0].address + "</li>");
+        });
+
+        $("#contacts-view").show();
+    }
+
+    /**
+    * Call the Contacts API and retrieve the (default) first 25 contacts
+    * @param A valid access token for calling the API with
+    * @return An object representing the contacts
+    */
+    var getContactEmails = function (access_token) {
+        $.getJSON('https://www.google.com/m8/feeds/contacts/default/full/?access_token=' +
+                                access_token + "&alt=json",
+                                function (result) {
+                                    displayContactEmails(result.feed.entry);
+                                }
+                            );
+    }
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+
+            $("#button-login").click(function () {
+                Office.context.ui.displayDialogAsync("https://localhost:44300/account.html", { width: 50, height: 50, requireHTTPS: true }, function (asyncResult) {
+                    var dialog = asyncResult.value;
+                    dialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, function (asyncResult) {
+                        if (asyncResult.message.indexOf("signin#") === 0) {
+                            var data = JSON.parse(asyncResult.message.substring(7));
+
+                            getContactEmails(data.access_token);
+
+                            dialog.close();
+                        }
+                    });
+                });
+            });
+        });
+    };
+
+})(window.app || (window.app = {}))
